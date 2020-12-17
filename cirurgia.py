547604b3cfd22011fd4c7c6633c5dfa001f3964d
Rodrigo de Sapienza Luna <rodrigo.luna@outlook.com.br>
2020-12-17 00:28:02 -0300

    Inicio da implementacao do ACO com o problema dado

--- cirurgia.py
+++ cirurgia.py
@@ -2,6 +2,127 @@ import numpy as np
 import random
 
 
+
+
+class Cirurgia:
+
+	def __init__(self,id,p,w,e,h,tc):
+		self.id = id
+		self.p = p
+		self.w = w
+		self.e = e
+		self.h = h
+		self.tc = tc
+		self.sala = -1
+	#Overloading do 
+	def __gt__(self,other):
+		if(self.p < other.p):
+			return True
+		elif(self.p == other.p):
+			if(self.w < other.w):
+				return True
+			elif(self.tc <= other.tc):
+				return True
+		return False
+
+
+	def setSala(self, s):
+		self.sala = s
+
+	def setDia(self,d):
+		self.dia = d
+	def setSemana(self,s):
+		self.semana = s
+
+	#Inicio da cirurgia e fim da cirurgia, inclusive
+	def setTempo(self,a,b):
+		self.tc_inicio = a
+		self.tc_fim = b
+
+	def setCirurgiao(self,cir):
+		self.cirurgiao = cir
+
+class Cirurgiao:
+
+	def __init__ (self,id,especialidade):
+		self.id = id
+		self.e = especialidade
+		self.hdia = 24
+		self.hsemana = 100
+
+	def novoDia():
+		self.hdia = 24
+	def novaSemana():
+		self.hsemana = 100
+
+
+class Sala:
+	def __init__(self,id):
+		self.id = id
+		self.disponivel = 0
+
+	#Esse é o tempo que vai estar disponível após a limpeza da sala. no tempo Disponível a sala estará liberada
+	#Por isso soma-se 3 unidades, h unidades do uso, + 2 unidades para limpeza, +1 para o tempo de disponibilidade
+	def setHora(self, h):
+		self.disponivel = h+3
+
+	def novoDia():
+		self.disponivel = 0
+
+
+#Funcao objetivo, dado a entrada da instancia, calcula o valor atual das cirurgias e as suas respectivas datas em que foram agendadas. 
+def FO():
+	return None
+
+#Funcao verifica se a instancia está dentro do Bloco do Problema. 
+def verificaInstancia():
+	return None
+
+
+
+def agenda(tempo,Cirurgias,Salas,Cirurgioes):
+	Cirurgias.sort()
+
+
+	for s in range(len(Salas)):
+		#Se a sala ja foi toda ocupada, nao adianta tentar ocupá-la
+		if(Salas[s].disponivel == 46):
+			continue
+
+		#Para cada cirurgia, tenta agendar 
+		for cirurgia in Cirurgias:
+
+
+'''Algoritmo que agenda as cirurgias de forma gulosa, dando prioridade a algumas caracteristicas, retorna a configuração final das cirurgias agendadas
+Tem como entrada: s- número de salas do problema, a lista com as Cirurgias, Cirurgioes e Salas.
+'''
+def agendaGreedy(s,Cirurgias,Salas,Cirurgioes ):
+	tempo_atual = 1
+	dia_atual = 1
+	semana_atual = 1
+	cirurgias_realizadas = 0
+
+	while( cirurgias_realizadas < len(Cirurgias) ):
+		agenda(tempoAtual,Cirurgias,Salas,Cirurgioes)
+
+		tempo_atual+= 1
+
+		#Acabou 
+		if(tempo_atual == 46):
+			zeraDisponibilidade()
+			tempo_atual = 1
+			dia_atual += 1
+		if(dia_atual==5):
+			dial_atual = 1
+			semana_atual += 1
+
+
+
+
+
+
+
+	return None
 '''
 -- Uma formiga deve ser capaz de iniciar a sua busca a partir de uma cidade específica
 -- Lembrar de todas as cidades visitadas
@@ -96,6 +217,10 @@ def read_graph(N,graph):
 		x = input().split(" ")
 		for j in range(N):
 			graph[i][j] = float(x[j])
+
+
+
+
 
 
 def main():
