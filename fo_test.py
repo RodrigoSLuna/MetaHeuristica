554c08471e5d4ede8d95b52ea5626fb3d5eee789
Vilma Bezerra Alves <vilmabezerraalves@gmail.com>
2021-01-09 19:57:38 -0300

    Add second test and fix FO

--- fo_test.py
+++ fo_test.py
@@ -76,7 +76,7 @@ def FO(cirurgias, salas):
         if cirurgia.p == 1 and cirurgia.dia > 1:
             p1 = 1
 
-        penalty += 10 * (pow(cirurgia.w + 2, cirurgia.dia)) * p1
+        penalty += pow(10 * (cirurgia.w + 2), cirurgia.dia) * p1
 
         if cirurgia.dia != -1:
             penalty += (pow(cirurgia.w + 2 + cirurgia.dia, 2) + pow(
@@ -90,7 +90,30 @@ def FO(cirurgias, salas):
     return penalty
 
 
-def test_fo_toy1():
+def test_fo_toy1_urgency_delay():
+    cirurgias, salas = set_toy1_original_solution()
+    cirurgias[0].sala = 1
+    cirurgias[0].tc_inicio = 42
+    cirurgias[0].tc_fim = 46
+    cirurgias[0].dia = 2
+    cirurgias[0].semana = 1
+
+    cirurgias[7].sala = 1
+    cirurgias[7].tc_inicio = 1
+    cirurgias[7].tc_fim = 5
+    cirurgias[7].dia = 1
+    cirurgias[7].semana = 1
+
+    return FO(cirurgias, salas)
+
+
+def test_fo_toy1_original():
+    cirurgias, salas = set_toy1_original_solution()
+
+    return FO(cirurgias, salas)
+
+
+def set_toy1_original_solution():
     cirurgias = [
         Cirurgia(1, 1, 1, 1, 1, 5),
         Cirurgia(2, 1, 1, 1, 1, 13),
@@ -101,60 +124,54 @@ def test_fo_toy1():
         Cirurgia(7, 2, 8, 2, 3, 11),
         Cirurgia(8, 3, 5, 2, 4, 5)
     ]
-
     salas = [Sala(1)]
-
     cirurgias[0].sala = 1
     cirurgias[0].tc_inicio = 1
     cirurgias[0].tc_fim = cirurgias[0].tc
     cirurgias[0].dia = 1
     cirurgias[0].semana = 1
-
     cirurgias[1].sala = 1
     cirurgias[1].tc_inicio = 8
     cirurgias[1].tc_fim = 20
     cirurgias[1].dia = 1
     cirurgias[1].semana = 1
-
     cirurgias[2].sala = 1
     cirurgias[2].tc_inicio = 23
     cirurgias[2].tc_fim = 30
     cirurgias[2].dia = 1
     cirurgias[2].semana = 1
-
     cirurgias[3].sala = 1
     cirurgias[3].tc_inicio = 33
     cirurgias[3].tc_fim = 43
     cirurgias[3].dia = 1
     cirurgias[3].semana = 1
-
     cirurgias[4].sala = 1
     cirurgias[4].tc_inicio = 1
     cirurgias[4].tc_fim = 10
     cirurgias[4].dia = 2
     cirurgias[4].semana = 1
-
     cirurgias[5].sala = 1
     cirurgias[5].tc_inicio = 26
     cirurgias[5].tc_fim = 39
     cirurgias[5].dia = 2
     cirurgias[5].semana = 1
-
     cirurgias[6].sala = 1
     cirurgias[6].tc_inicio = 13
     cirurgias[6].tc_fim = 23
     cirurgias[6].dia = 2
     cirurgias[6].semana = 1
-
     cirurgias[7].sala = 1
     cirurgias[7].tc_inicio = 42
     cirurgias[7].tc_fim = 46
     cirurgias[7].dia = 2
     cirurgias[7].semana = 1
-
-    print(FO(cirurgias, salas))
+    return cirurgias, salas
 
 
 if __name__ == '__main__':
     print("I got here!")
-    test_fo_toy1()
+    print(test_fo_toy1_original())
+    print(test_fo_toy1_urgency_delay())
+
+
+
